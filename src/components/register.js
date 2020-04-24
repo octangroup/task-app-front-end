b1b3ee55c9b3b562df8ddb903f0658f834cb5773
hervengenzi <ngenziherve@octagrp.com>
2020-04-24 07:13:59 +0300

    Add User Registration form

---
+++ src/components/register.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import {} from 'react-native';
+
+export default class Register extends Component {
+    constructor(props){
+        super(props);
+    }
+}
