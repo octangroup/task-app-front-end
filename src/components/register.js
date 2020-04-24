d3c4edd450935c0c7fb06db75319c830cdb6ab3c
hervengenzi <ngenziherve@octagrp.com>
2020-04-24 07:54:43 +0300

    Register Form

--- src/components/register.js
+++
@@ -1,8 +0,0 @@
-import React, {Component} from 'react';
-import {} from 'react-native';
-
-export default class Register extends Component {
-    constructor(props){
-        super(props);
-    }
-}
