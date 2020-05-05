8c3fdda486d5f1933c2d39d3c8f94fc69db9a3dd
hervengenzi <ngenziherve@octagrp.com>
2020-05-05 14:36:52 +0300

    Add required packages and change some routes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,21 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
-export default class Home extends Component{
-        render() {
-            return(
-                
-            );
-        }
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>First Page</Text>;
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  text: {
+    color: 'red',
+  },
+});
