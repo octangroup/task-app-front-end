fe20a1ae12ad753e408e6413bdb426538f646435
hervengenzi <ngenziherve@octagrp.com>
2020-05-06 12:49:55 +0300

    Create user task page

---
+++ src/pages/TaskForm.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+
+export default class TaskForm extends Component {
+  render() {
+    return <View />;
+  }
+}
+
+const styles = StyleSheet.create({});
